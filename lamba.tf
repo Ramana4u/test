33006465028454ad99efdd1a175ba297ff56d7ab
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-12 21:50:33 +0530

    Update lamba.tf

--- lamba.tf
+++ lamba.tf
@@ -1,10 +1,30 @@
 provider "aws"{
   region="us-east-2"
 }
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
-  role          = "arn:aws:iam::697613968254:role/service-role/demo-lamda-role-ywf15xwb"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
  source_code_hash = filebase64sha256("lambda_function_payload.zip")
 runtime = "python3.9"
